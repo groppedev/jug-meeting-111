28659549e95af670ea12a48b74ba4ca2120a6dde
Massimo Groppelli <massimo.groppelli@gmail.com>
2019-03-16 17:41:44 +0100

    Changed benchmark

--- src/main/java/groppedev/jug/meeting111/jmh/SpringBenchmark.java
+++ src/main/java/groppedev/jug/meeting111/jmh/SpringBenchmark.java
@@ -5,17 +5,17 @@ import java.util.concurrent.TimeUnit;
 import javax.inject.Provider;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.CompilerControl;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
-import org.openjdk.jmh.profile.GCProfiler;
-import org.openjdk.jmh.profile.StackProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -33,11 +33,16 @@ import groppedev.jug.meeting111.SMTPTransport;
  * 
  * - Set fork(0) for debugging
  * 
+ * @see http://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
+ * @see https://mechanical-sympathy.blogspot.com/2011/11/biased-locking-osr-and-benchmarking-fun.html?m=1
+ * 
  * @author Groppelli Massimo
  */
 @SuppressWarnings({"static-method", "unchecked"})
+@BenchmarkMode(Mode.SingleShotTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@CompilerControl(CompilerControl.Mode.EXCLUDE)
 @State(Scope.Benchmark)
 public class SpringBenchmark 
 {
@@ -59,6 +64,7 @@ public class SpringBenchmark
 	{
 		applicationContext.close();
 	}
+	
 	@Benchmark
 	public void singletonFactory(Blackhole bh)
 	{
@@ -84,24 +90,17 @@ public class SpringBenchmark
 	{
 		bh.consume(applicationContext.getBean("jug.emailer.proto"));
 	}
-	@Benchmark
+    @Benchmark
 	public void prototypeProviderSpring(Blackhole bh)
 	{
 		bh.consume(((Provider<Emailer>) applicationContext.getBean("jug.emailer.proto.provider")).get());
 	}
-
 	public static void main(String...args) throws RunnerException
 	{
 		Options opt = new OptionsBuilder()
-				.mode(Mode.Throughput)
-				.timeUnit(TimeUnit.MILLISECONDS)
 				.include(SpringBenchmark.class.getSimpleName())
 				.forks(1)
-				.threads(5)
-				.shouldDoGC(true)                 
-				.shouldFailOnError(true)
-				.addProfiler(GCProfiler.class)
-				.addProfiler(StackProfiler.class)
+				.jvmArgsAppend("-XX:-UseBiasedLocking")
 				.build();
 		new Runner(opt).run();
 	}
